c9c1c3e7d90eb202766bcbb16050b02398ddfb42
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-27 20:42:02 +0800

    Add stockfish js engine for in-browser analysis engine

---
+++ app/javascript/workers/stockfish_engine.js
@@ -0,0 +1,115 @@
+import d from '../dispatcher'
+
+const wasmSupported = typeof WebAssembly === 'object' && WebAssembly.validate(Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00))
+
+const SEARCH_DEPTH = 10
+
+class StockfishEngine {
+
+  constructor(options = {}) {
+    this.initialized = false
+    this.multipv = options.multipv || 1
+    this.initStockfish()
+  }
+
+  initStockfish() {
+    if (this.initialized) {
+      return
+    }
+    this.stockfish = new Worker(`/assets/stockfish${wasmSupported ? '.wasm' : ''}.js`)
+    if (this.multipv > 1) {
+      this.stockfish.postMessage('setoption name MultiPV value ' + this.multipv)
+    }
+    this.stockfish.postMessage('uci')
+    this.debugMessages()
+    this.initialized = true
+  }
+
+  debugMessages() {
+    this.stockfish.addEventListener('message', e => console.log(e.data))
+  }
+
+  analyze(fen, options = {}, callback = () => {}) {
+    let targetDepth = +options.depth || SEARCH_DEPTH
+    this.stockfish.postMessage('position fen ' + fen)
+    this.emitEvaluationWhenDone(fen, targetDepth, callback)
+    this.stockfish.postMessage('go depth ' + targetDepth)
+  }
+
+  emitEvaluationWhenDone(fen, depth, callback) {
+    const start = new Date()
+    const targetDepth = depth
+    const targetMultiPv = this.multipv
+
+    const done = (state) => {
+      callback({
+        fen,
+        eval: state.eval
+      })
+      this.stockfish.removeEventListener('message', processOutput)
+    }
+
+    // Modified from lila/ui/analyse/src/ceval/stockfishProtocol.js
+    //
+    let state
+    let processOutput = (e) => {
+      if (e.data.indexOf('bestmove ') === 0) {
+        return
+      }
+
+      var matches = e.data.match(/depth (\d+) .*multipv (\d+) .*score (cp|mate) ([-\d]+) .*nps (\d+) .*pv (.+)/)
+      if (!matches) {
+        return
+      }
+
+      var depth = parseInt(matches[1])
+      if (depth < targetDepth) {
+        return
+      }
+
+      var multiPv = parseInt(matches[2])
+      var cp, mate
+
+      if (matches[3] === 'cp') {
+        cp = parseFloat(matches[4])
+      } else {
+        mate = parseFloat(matches[4])
+      }
+
+      if (fen.indexOf('w') === -1) {
+        if (matches[3] === 'cp') cp = -cp
+        else mate = -mate
+      }
+
+      if (multiPv === 1) {
+        state = {
+          eval: {
+            depth: depth,
+            nps: parseInt(matches[5]),
+            best: matches[6].split(' ')[0],
+            cp: cp,
+            mate: mate,
+            pvs: []
+          }
+        }
+      } else if (!state || depth < state.eval.depth) return // multipv progress
+
+      state.eval.pvs[multiPv - 1] = {
+        cp: cp,
+        mate: mate,
+        pv: matches[6],
+        best: matches[6].split(' ')[0]
+      }
+
+      if (multiPv === targetMultiPv && state.eval.depth === targetDepth) {
+        done(state)
+      }
+    }
+
+    this.stockfish.addEventListener('message', processOutput)
+  }
+}
+
+const stockfish = new StockfishEngine
+
+export default stockfish
