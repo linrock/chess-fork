a3024c6b9efc450372f397a34d7919013e3250ff
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-28 19:17:10 +0800

    Start using vue.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const vue =  require('./loaders/vue')
 const typescript =  require('./loaders/typescript')
 
 environment.loaders.append('typescript', typescript)
+environment.loaders.append('vue', vue)
 module.exports = environment
