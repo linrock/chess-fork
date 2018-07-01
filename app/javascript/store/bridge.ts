c3a3fdb9b7ea29d0dde52674dcd7d819ea183de4
Linmiao Xu <linmiao.xu@gmail.com>
2018-07-01 15:31:06 +0800

    Move backbone/vuex event bridge to a separate file

---
+++ app/javascript/store/bridge.ts
@@ -0,0 +1,51 @@
+import Backbone from 'backbone'
+
+import { FEN } from '../types'
+import { chess } from '../chess_mechanism'
+import { world } from '../world_state'
+import analysisCache from '../analysis/cache'
+
+// temporary bridge between backbone events and vuex store
+const initBackboneBridge = state => {
+  const currentFen = (): FEN => chess.getPosition(state.positionIndex)
+
+  const listener = <any>Object.assign({}, Backbone.Events)
+
+  listener.listenTo(world, "change:i", (_, i) => {
+    state.positionIndex = i
+    const fen = currentFen()
+    const analysisOptions = {
+      multipv: state.multipv,
+      depth: state.depth
+    }
+    chess.trigger("analysis:enqueue", fen, analysisOptions)
+  })
+  listener.listenTo(chess, "change:j", (_, j) => state.variationIndex = j)
+  listener.listenTo(chess, "change:k", (_, k) => state.variationPositionIndex = k)
+  listener.listenTo(chess, "change:mode", (_, mode) => state.mode = mode)
+
+  listener.listenTo(chess, "analysis:complete", fen => {
+    const analysisOptions = {
+      multipv: state.multipv,
+      depth: state.depth
+    }
+    const analysis = analysisCache.get(fen, analysisOptions)
+    if (fen === chess.getPosition(state.positionIndex)) {
+      state.currentAnalysis = analysis
+    }
+  })
+  listener.listenTo(chess, "analysis:options:change", () => {
+    const fen = currentFen()
+    const { multipv, depth } = state
+    chess.trigger("analysis:enqueue", fen, { multipv, depth })
+  })
+
+  listener.listenTo(world, "change:moves", (_, moves) => {
+    state.moves = moves.toArray()
+  })
+  listener.listenTo(world, "change:positions", (_, positions) => {
+    state.positions = positions.toArray()
+  })
+}
+
+export default initBackboneBridge
--- app/javascript/store/index.ts
+++ app/javascript/store/index.ts
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Backbone from 'backbone'
 
 import { FEN, SanMove } from '../types'
-import { chess } from '../chess_mechanism'
-import { world } from '../world_state'
 import Analysis from '../analysis/models/analysis'
-import analysisCache from '../analysis/cache'
 import { defaultAnalysisOptions } from '../analysis/options'
+import initBackboneBridge from './bridge'
 
 Vue.use(Vuex)
 
@@ -33,51 +30,15 @@ const state: GlobalState = Object.assign({}, defaultAnalysisOptions, {
   currentAnalysis: null,
 })
 
-const currentFen = (): FEN => chess.getPosition(state.positionIndex)
-
-// temporary bridge between backbone events and vuex store
-const listener = <any>Object.assign({}, Backbone.Events)
-listener.listenTo(world, "change:i", (_, i) => {
-  state.positionIndex = i
-  const fen = currentFen()
-  const analysisOptions = {
-    multipv: state.multipv,
-    depth: state.depth
-  }
-  chess.trigger("analysis:enqueue", fen, analysisOptions)
-})
-listener.listenTo(chess, "change:j", (_, j) => state.variationIndex = j)
-listener.listenTo(chess, "change:k", (_, k) => state.variationPositionIndex = k)
-listener.listenTo(chess, "change:mode", (_, mode) => state.mode = mode)
-
-listener.listenTo(chess, "analysis:complete", fen => {
-  const analysisOptions = {
-    multipv: state.multipv,
-    depth: state.depth
-  }
-  const analysis = analysisCache.get(fen, analysisOptions)
-  if (fen === chess.getPosition(state.positionIndex)) {
-    state.currentAnalysis = analysis
-  }
-})
-listener.listenTo(chess, "analysis:options:change", () => {
-  const fen = currentFen()
-  const { multipv, depth } = state
-  chess.trigger("analysis:enqueue", fen, { multipv, depth })
-})
-
-listener.listenTo(world, "change:moves", (_, moves) => {
-  state.moves = moves.toArray()
-})
-listener.listenTo(world, "change:positions", (_, positions) => {
-  state.positions = positions.toArray()
-})
-
 const store = new Vuex.Store({
   state,
   getters: {
-
+    currentFen(state): FEN {
+      return state.positions[state.positionIndex]
+    }
   }
 })
+
+initBackboneBridge(state)
 
 export default store
